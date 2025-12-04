2084b93e28b7832b808716e6a7e790d9bc6e2576
Dmytro Minyaylo <dimon9660@gmail.com>
2025-12-04 09:39:15 +0200

    Format jobs spider according to flake8 rules

--- dou_scraper/spiders/jobs.py
+++ dou_scraper/spiders/jobs.py
@@ -18,7 +18,9 @@ class JobsSpider(scrapy.Spider):
     }
 
     def parse(self, response):
-        for job_link in response.css("div.vacancy a.vacancy-title::attr(href)").getall():
+        for job_link in response.css(
+                "div.vacancy a.vacancy-title::attr(href)"
+        ).getall():
             yield response.follow(job_link, callback=self.parse_job)
 
         next_page = response.css("a.pagination-next::attr(href)").get()
@@ -27,12 +29,33 @@ class JobsSpider(scrapy.Spider):
 
     def parse_job(self, response):
         item = DouScraperItem()
+
         item["title"] = response.css("h1.h1::text").get(default="").strip()
-        item["company"] = response.css("div.terms a::text").get(default="").strip()
-        item["location"] = response.css("div.place::text").get(default="").strip()
-        item["experience"] = response.css("div.terms span.experience::text").get(default="").strip()
-        item["salary"] = response.css("div.salary span::text").get(default="").strip()
+
+        item["company"] = response.css(
+            "div.terms a::text"
+        ).get(default="").strip()
+
+        item["location"] = response.css(
+            "div.place::text"
+        ).get(default="").strip()
+
+        item["experience"] = response.css(
+            "div.terms span.experience::text"
+        ).get(default="").strip()
+
+        item["salary"] = response.css(
+            "div.salary span::text"
+        ).get(default="").strip()
+
         item["link"] = response.url
-        item["description"] = response.css("div.vacancy-description").get(default="").strip()
-        item["date_posted"] = response.css("time::attr(datetime)").get(default="").strip()
+
+        item["description"] = response.css(
+            "div.vacancy-description"
+        ).get(default="").strip()
+
+        item["date_posted"] = response.css(
+            "time::attr(datetime)"
+        ).get(default="").strip()
+
         yield item
